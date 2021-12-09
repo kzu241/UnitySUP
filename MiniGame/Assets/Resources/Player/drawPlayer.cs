9b452623a0d8cd8182af5fe5cbcbb08ef566a7b9
kzu241 <67955570+kzu241@users.noreply.github.com>
2021-12-09 13:09:28 +0900

    ohashi

---
+++ MiniGame/Assets/Resources/Player/drawPlayer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drawPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject player;
+    void Start( ) {
+       player  = (GameObject)Resources.Load( "Player/player" );
+        Instantiate( player, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity );
+    }
+
+    // Update is called once per frame
+    void Update( ){
+        
+    }
+}
