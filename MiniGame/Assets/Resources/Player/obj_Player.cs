ea723b716fc9d712aa526dd75fdfff1c5b71af59
kzu241 <67955570+kzu241@users.noreply.github.com>
2021-12-09 15:58:10 +0900

    ohashi

--- MiniGame/Assets/Resources/Player/obj_Player.cs
+++ MiniGame/Assets/Resources/Player/obj_Player.cs
@@ -6,9 +6,10 @@ public class obj_Player : MonoBehaviour
 {
     // Start is called before the first frame update
     GameObject player;
+    GameObject clone_position;
     void Start( ) {
        player = (GameObject)Resources.Load( "Player/player" );
-        Instantiate( player, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity );
+        clone_position = Instantiate( player, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity );
     }
 
     // Update is called once per frame
@@ -18,6 +19,7 @@ public class obj_Player : MonoBehaviour
     void  move( ) {
         if ( Input.GetKey(KeyCode.UpArrow ) ){
             transform.position = new Vector3( 0f, transform.position.x + 0.1f, 0f );
-        } 
+        }
+        clone_position = new Vector3( 0.0f, transform.position.x, transform.position.z ) );
     }
 }
