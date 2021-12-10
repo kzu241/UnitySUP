3c7007300d882d3e744bcf9fd3053c20b2c545df
kzu241 <67955570+kzu241@users.noreply.github.com>
2021-12-10 14:48:04 +0900

    ohashi

--- MiniGame/Assets/Resources/Player/obj_Player.cs
+++ MiniGame/Assets/Resources/Player/obj_Player.cs
@@ -5,38 +5,47 @@ using UnityEngine;
 public class obj_Player : MonoBehaviour
 {
     // Start is called before the first frame update
-    Vector3 player_pos = new Vector3( 0f, 0f, 0f );
+    Vector3 player_pos = new Vector3( 0.0f, 0.0f, 0.0f );
+    Vector3 ground_pos = new Vector3( 0.0f, 0.0f, 0.0f );
+    Vector3 now_pos;
+    Transform player;
+    float gravity_power = -0.02f;
     void Start( ) {
+        //GameObject.Find("Player/player").transform.position = new Vector3( player.x, player.y, player.z);
+
     }
 
     // Update is called once per frame
     void Update( ) {
-        player_pos = GameObject.Find("Player/player").transform.position;
-        //GameObject.Find("Player/player").transform.position = new Vector3( player.x, player.y, player.z);
-        if ( player_pos.y <= 0 ){
-            transform.Translate( 0f, 0f, 0f );
-        }else{
-            transform.Translate( 0.0f, -0.2f, 0.0f );
-        }
+        gravity( );
         move( );
+    }
+
+    void gravity( ){
+        player = this.transform;
+        now_pos = player.position;
+        float pos_y = now_pos.y;
+        float ground_get_pos = ground_pos.y;
+        if ( pos_y <= ground_get_pos ) {
+            transform.Translate( 0.0f, 0.0f, 0.0f );
+        } else {
+            transform.Translate( 0.0f, gravity_power, 0.0f );
+            gravity_power -= 0.01f;
+        }
     }
         
     void move( ){
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            transform.Translate(0f, 0f, 0.1f);
+        if ( Input.GetKey( KeyCode.UpArrow ) ) {
+            transform.Translate( 0f, 0f, 0.1f );
         }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.Translate(0.0f, 0f, -0.1f);
+        if ( Input.GetKey( KeyCode.DownArrow ) ) {
+            transform.Translate( 0.0f, 0f, -0.1f );
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.Translate(-0.1f, 0f, 0f);
+        if ( Input.GetKey( KeyCode.LeftArrow ) ) {
+            transform.Translate( -0.1f, 0f, 0f );
         }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.Translate(0.1f, 0f, 0f);
+        if ( Input.GetKey( KeyCode.RightArrow ) ) {
+            transform.Translate( 0.1f, 0f, 0f );
         }
     }
 }
--- MiniGame/Assets/Resources/Player/obj_createPlayer.cs
+++ MiniGame/Assets/Resources/Player/obj_createPlayer.cs
@@ -6,7 +6,7 @@ public class obj_createPlayer : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject player;
-    public Vector3 clone_position = new Vector3( 1.0f, 5.0f, 0.0f );
+    public Vector3 clone_position = new Vector3( 1.0f, 30.0f, 0.0f );
     void Start( ) {
        player = (GameObject)Resources.Load( "Player/player" );
         Instantiate( player, clone_position, Quaternion.identity );
