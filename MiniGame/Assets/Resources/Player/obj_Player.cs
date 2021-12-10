be81972251d9d0eafa146208d7c40f0eee4adda4
kzu241 <67955570+kzu241@users.noreply.github.com>
2021-12-10 13:41:24 +0900

    ohashi

--- MiniGame/Assets/Resources/Player/obj_Player.cs
+++ MiniGame/Assets/Resources/Player/obj_Player.cs
@@ -4,27 +4,39 @@ using UnityEngine;
 
 public class obj_Player : MonoBehaviour
 {
-
     // Start is called before the first frame update
+    Vector3 player_pos = new Vector3( 0f, 0f, 0f );
     void Start( ) {
-        
     }
 
     // Update is called once per frame
     void Update( ) {
-        if ( Input.GetKey( KeyCode.UpArrow ) ){
-            transform.Translate( 0f, 0f, 0.1f );
+        player_pos = GameObject.Find("Player/player").transform.position;
+        //GameObject.Find("Player/player").transform.position = new Vector3( player.x, player.y, player.z);
+        if ( player_pos.y <= 0 ){
+            transform.Translate( 0f, 0f, 0f );
+        }else{
+            transform.Translate( 0.0f, -0.2f, 0.0f );
         }
-        if ( Input.GetKey( KeyCode.DownArrow ) ) {
-            transform.Translate( 0.0f, 0f, -0.1f );
+        move( );
+    }
+        
+    void move( ){
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0f, 0f, 0.1f);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0.0f, 0f, -0.1f);
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Translate( -0.1f, 0f, 0f );
+            transform.Translate(-0.1f, 0f, 0f);
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Translate( 0.1f, 0f, 0f );
+            transform.Translate(0.1f, 0f, 0f);
         }
     }
 }
--- MiniGame/Assets/Resources/Player/obj_createPlayer.cs
+++ MiniGame/Assets/Resources/Player/obj_createPlayer.cs
@@ -6,29 +6,9 @@ public class obj_createPlayer : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject player;
-    Vector3 clone_position = new Vector3( 1.0f, 10.0f, 0.0f );
+    public Vector3 clone_position = new Vector3( 1.0f, 5.0f, 0.0f );
     void Start( ) {
        player = (GameObject)Resources.Load( "Player/player" );
         Instantiate( player, clone_position, Quaternion.identity );
-    }
-
-    // Update is called once per frame
-    void Update( ){
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            transform.position = new Vector3(0f, 0f, 1.0f);
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.position = new Vector3(0.0f, 0f, 1.0f);
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.position = new Vector3(-1.0f, 0f, 0f);
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.position = new Vector3(1.0f, 0f, 0f);
-        }
     }
 }
