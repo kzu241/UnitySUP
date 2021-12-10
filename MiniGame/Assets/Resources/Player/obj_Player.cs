97ee184b5459895af0b074f1969401ae1449e181
kzu241 <67955570+kzu241@users.noreply.github.com>
2021-12-10 11:21:15 +0900

    okada

--- MiniGame/Assets/Resources/Player/obj_Player.cs
+++ MiniGame/Assets/Resources/Player/obj_Player.cs
@@ -4,22 +4,27 @@ using UnityEngine;
 
 public class obj_Player : MonoBehaviour
 {
+
     // Start is called before the first frame update
-    GameObject player;
-    GameObject clone_position;
     void Start( ) {
-       player = (GameObject)Resources.Load( "Player/player" );
-        clone_position = Instantiate( player, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity );
+        
     }
 
     // Update is called once per frame
-    void Update( ){
-        move( );
-    }
-    void  move( ) {
-        if ( Input.GetKey(KeyCode.UpArrow ) ){
-            transform.position = new Vector3( 0f, transform.position.x + 0.1f, 0f );
+    void Update( ) {
+        if ( Input.GetKey( KeyCode.UpArrow ) ){
+            transform.Translate( 0f, 0f, 0.1f );
         }
-        clone_position = new Vector3( 0.0f, transform.position.x, transform.position.z ) );
+        if ( Input.GetKey( KeyCode.DownArrow ) ) {
+            transform.Translate( 0.0f, 0f, -0.1f );
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate( -0.1f, 0f, 0f );
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate( 0.1f, 0f, 0f );
+        }
     }
 }
