235ebb6f617e54e8080d724e58bef1048f5793ab
kzu241 <67955570+kzu241@users.noreply.github.com>
2021-12-11 22:05:42 +0900

    ohashi

---
+++ MiniGame/Assets/Resources/Item/obj_createItem.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obj_createItem : MonoBehaviour {
+    public GameObject item;
+    Vector3 item_position = new Vector3( 5.0f, 0.5f, 0f );
+
+        Quaternion rotation = Quaternion.Euler(0, 0, 45);
+    void Start( ) {
+        item = ( GameObject )Resources.Load( "Item/item" );
+        Instantiate(item, item_position, rotation);
+        
+    }
+        // Update is called once per frame
+        void Update( ) {
+        
+    }
+}
