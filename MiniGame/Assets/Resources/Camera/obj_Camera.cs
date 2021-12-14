9df363812459e7289bbb7b61d41876dc97dc6600
kzu241 <67955570+kzu241@users.noreply.github.com>
2021-12-14 16:04:31 +0900

    ohashi

--- MiniGame/Assets/Resources/Camera/obj_Camera.cs
+++ MiniGame/Assets/Resources/Camera/obj_Camera.cs
@@ -7,13 +7,21 @@ public class obj_Camera : MonoBehaviour
     private GameObject player;
     private Vector3 now_camera_pos;
 
+    GameObject main_camera;
+    Vector3 camera_pos = new Vector3( 5.0f, 15.0f, -13.0f ) ;
+    Quaternion rotation = Quaternion.Euler( 50.0f, 0.0f, 0.0f );
     // Use this for initialization
     void Start( ) {
-        player = GameObject.FindGameObjectWithTag("Player");
+        main_camera = (GameObject)Resources.Load( "Camera/Main Camera" );
+        Instantiate( main_camera, camera_pos, rotation );
+        player = GameObject.FindGameObjectWithTag( "Player" );
     }
 
     void Update( ) {
+        if( player == null ){
+            return;
+        }
         now_camera_pos = player.transform.position;
-       this.transform.position = now_camera_pos;
+        camera_pos = now_camera_pos;
     }
 }
--- MiniGame/Assets/Resources/Item/obj_createItem.cs
+++ MiniGame/Assets/Resources/Item/obj_createItem.cs
@@ -6,7 +6,7 @@ public class obj_createItem : MonoBehaviour {
     public GameObject item;
     Vector3 item_position = new Vector3( 5.0f, 0f, 5f );
 
-        Quaternion rotation = Quaternion.Euler(0, 0, 45);
+    Quaternion rotation = Quaternion.Euler(0, 0, 45);
     void Start( ) {
         item = ( GameObject )Resources.Load( "Item/item" );
         Instantiate(item, item_position, rotation);
