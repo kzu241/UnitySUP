7cd6d39ba187df2cc72110ca7ca953691fbb4002
kzu241 <67955570+kzu241@users.noreply.github.com>
2022-01-18 11:44:55 +0900

    ohashi

--- MiniGame/Assets/Script/script_Controller.cs
+++ MiniGame/Assets/Script/script_Controller.cs
@@ -23,17 +23,24 @@ public class script_Controller : MonoBehaviour {
         return Instantiate( prefab, pos, Quaternion.identity );
     }
     void Start( ) {
-        _player_game_object = loadPrefab("prefab_player", new Vector3( 1.0f, 5.0f, 0.0f ) );
-        _floor_game_object = loadPrefab("prefab_Floor", new Vector3( 5.0f, 0.0f, 5.0f ) );
-        _front_game_object = loadPrefab("prefab_FrontWall", new Vector3( 5.0f, 1.0f, 15.0f ) );
-        _back_game_object = loadPrefab("prefab_BackWall", new Vector3( 5.0f, 1.0f, -5.0f ) );
-        _right_game_object = loadPrefab("prefab_RightWall", new Vector3( 15.0f, 1.0f, 5.0f ) );
-        _left_game_object = loadPrefab("prefab_LeftWall", new Vector3( -5.0f, 1.0f, 5.0f ) );
-        _item_game_object = loadPrefab("prefab_item", new Vector3( 5.0f, 1.0f, 5f ) );
+        _player_game_object = loadPrefab("prefab_player", new Vector3( 1f, 5f, 0f ) );
+        _floor_game_object = loadPrefab("prefab_Floor", new Vector3( 5f, 0f, 5f ) );
+        _front_game_object = loadPrefab("prefab_FrontWall", new Vector3( 5f, 1f, 15f ) );
+        _back_game_object = loadPrefab("prefab_BackWall", new Vector3( 5f, 1f, -5f ) );
+        _right_game_object = loadPrefab("prefab_RightWall", new Vector3( 15f, 1f, 5f ) );
+        _left_game_object = loadPrefab("prefab_LeftWall", new Vector3( -5f, 1f, 5f ) );
+        _item_game_object = loadPrefab("prefab_Item", new Vector3( 5f, 1f, 5f ) );
 
         _front_game_object.transform.Rotate( 0f, 90f, 0f );
         _back_game_object.transform.Rotate( 0f, 90f, 0f );
         _item_game_object.transform.Rotate( 0f, 0f, 45f );
+
+        _player_game_object.GetComponent< Renderer >( ).material.color = Color.red;
+        _front_game_object.GetComponent< Renderer > ( ).material.color = Color.cyan;
+        _back_game_object.GetComponent< Renderer > ( ).material.color = Color.cyan;
+        _left_game_object.GetComponent< Renderer > ( ).material.color = Color.cyan;
+        _right_game_object.GetComponent< Renderer > ( ).material.color = Color.cyan;
+        _item_game_object.GetComponent< Renderer >( ).material.color = Color.yellow;
     }
    
     void Update( ) {
@@ -51,30 +58,24 @@ public class script_Controller : MonoBehaviour {
         Quaternion rotate_y = item_rotation * _item_game_object.transform.rotation;
         _item_game_object.transform.rotation = rotate_y;
     }
-
     void movePlayer( ) {
         if ( Input.GetKey( KeyCode.UpArrow ) && !_stop_front ) {
             _speed = 7.0f * Time.deltaTime;
             _player_game_object.transform.Translate( 0, 0, _speed, Space.World );
-            moveForwardAnimation( );
         }
         if ( Input.GetKey( KeyCode.DownArrow ) && !_stop_back ) {
             _speed = -7.0f * Time.deltaTime;
             _player_game_object.transform.Translate( 0, 0, _speed, Space.World );
-            moveBackwardAnimation( );
         }
         if ( Input.GetKey( KeyCode.LeftArrow ) && !_stop_left ) {
             _speed = -7.0f * Time.deltaTime;
             _player_game_object.transform.Translate( _speed, 0, 0, Space.World );
-            moveLeftAnimation( );
         }
         if ( Input.GetKey( KeyCode.RightArrow ) && !_stop_right ) {
             _speed = 7.0f * Time.deltaTime;
             _player_game_object.transform.Translate( _speed, 0, 0, Space.World );
-            moveRightAnimation( );
         }
     }
-
     void removeItem( ) {
         float now_pos_x = _player_game_object.transform.position.x;
         float now_pos_z = _player_game_object.transform.position.z;
@@ -84,41 +85,12 @@ public class script_Controller : MonoBehaviour {
         float coll_z = changePositiveNumber( item_pos_z - now_pos_z );
         if ( coll_z <= 1.0f && coll_x <= 1.0f ) {
             Destroy( _item_game_object );
-            Debug.Log("‚«‚¿‚á[");
         }
     }
-
     float changePositiveNumber( float coll ) {
         if ( coll < 0 ) {
             coll *= -1f;
         }
         return coll;
     }
-
-    void OnCollisionEnter( Collision collision ) {
-            Debug.Log( "‚«‚¿‚á[" );
-    }
-
-    //Animation
-    void moveForwardAnimation( ) {
-        Quaternion z = Quaternion.AngleAxis( 300.0f * Time.deltaTime, Vector3.right );
-        Quaternion forward_move = z * _player_game_object.transform.rotation;
-        _player_game_object.transform.rotation = forward_move;
-    }
-    void moveBackwardAnimation( ) {
-        Quaternion z = Quaternion.AngleAxis( -300.0f * Time.deltaTime, Vector3.right );
-        Quaternion backward_move = z * _player_game_object.transform.rotation;
-        _player_game_object.transform.rotation = backward_move;
-    }
-    void moveLeftAnimation( ) {
-        Quaternion x = Quaternion.AngleAxis( 300.0f * Time.deltaTime, Vector3.forward );
-        Quaternion left_move = x * _player_game_object.transform.rotation;
-        _player_game_object.transform.rotation = left_move;
-    }
-    void moveRightAnimation( ) {
-        Quaternion x = Quaternion.AngleAxis( -300.0f * Time.deltaTime, Vector3.forward );
-        Quaternion right_move = x * _player_game_object.transform.rotation;
-        _player_game_object.transform.rotation = right_move;
-    }
-
 }
