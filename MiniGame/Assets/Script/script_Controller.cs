48fdd74db2bc0eadeb523c25f06c0758507baaab
kzu241 <67955570+kzu241@users.noreply.github.com>
2022-01-26 11:01:08 +0900

    ohashi

--- MiniGame/Assets/Script/script_Controller.cs
+++ MiniGame/Assets/Script/script_Controller.cs
@@ -12,13 +12,13 @@ public class script_Controller : MonoBehaviour {
         return Instantiate( prefab_data, pos, Quaternion.identity );
     }
     void Start( ) {
-        GameObject _floor_game_object = loadPrefab( "prefab_Floor", new Vector3( 5f, 0.5f, 5f ) );
-        GameObject _front_game_object = loadPrefab( "prefab_FrontWall", new Vector3( 5f, 1f, 15f ) );
-        GameObject _back_game_object = loadPrefab( "prefab_BackWall", new Vector3( 5f, 1f, -5f ) );
-        GameObject _right_game_object = loadPrefab( "prefab_RightWall", new Vector3( 15f, 1f, 5f ) );
-        GameObject _left_game_object = loadPrefab( "prefab_LeftWall", new Vector3( -5f, 1f, 5f ) );
-        _player_game_object = loadPrefab( "prefab_Player", new Vector3( 1f, 5f, 0f ) );
-        _item_game_object = loadPrefab( "prefab_Item", new Vector3( 5f, 1f, 5f ) );
+        GameObject _floor_game_object = loadPrefab( "prefab_Floor", new Vector3( 5.0f, 0.5f, 5.0f ) );
+        GameObject _front_game_object = loadPrefab( "prefab_FrontWall", new Vector3( 5.0f, 1.0f, 15.0f ) );
+        GameObject _back_game_object = loadPrefab( "prefab_BackWall", new Vector3( 5.0f, 1.0f, -5.0f ) );
+        GameObject _right_game_object = loadPrefab( "prefab_RightWall", new Vector3( 15.0f, 1.0f, 5.0f ) );
+        GameObject _left_game_object = loadPrefab( "prefab_LeftWall", new Vector3( -5.0f, 1.0f, 5.0f ) );
+        _player_game_object = loadPrefab( "prefab_Player", new Vector3( 1.0f, 5.0f, 0.0f ) );
+        _item_game_object = loadPrefab( "prefab_Item", new Vector3( 5.0f, 1.0f, 5.0f ) );
 
         _player_game_object.GetComponent<Renderer>( ).material.color = Color.red;
         _front_game_object.GetComponent<Renderer>( ).material.color = Color.cyan;
@@ -27,16 +27,16 @@ public class script_Controller : MonoBehaviour {
         _left_game_object.GetComponent<Renderer>( ).material.color = Color.cyan;
         _item_game_object.GetComponent<Renderer>( ).material.color = Color.yellow;
 
-        _left_game_object.transform.Rotate( 0f, 90f, 0f );
-        _right_game_object.transform.Rotate( 0f, 90f, 0f );
-        _item_game_object.transform.Rotate( 0f, 0f, 45f );
+        _left_game_object.transform.Rotate( 0.0f, 90.0f, 0.0f );
+        _right_game_object.transform.Rotate( 0.0f, 90.0f, 0.0f );
+        _item_game_object.transform.Rotate( 0.0f, 0.0f, 45.0f );
     }
    
     void Update( ) {
-        processItem( );
+        updateItem( );
         movePlayer( );
     }
-    void processItem( ) {
+    void updateItem( ) {
         if( _item_game_object != null ){
             rotationItem( );
             removeItem( );
@@ -48,10 +48,10 @@ public class script_Controller : MonoBehaviour {
         _item_game_object.transform.rotation = rotate_y;
     }
     void movePlayer( ) {
-        float speed = 20f * Time.deltaTime;
-        float x = 0f;
-        float z = 0f;
-        Rigidbody _rb_player = _player_game_object.transform.GetComponent<Rigidbody>( );
+        float speed = 20.0f * Time.deltaTime;
+        float x = 0.0f;
+        float z = 0.0f;
+        Rigidbody rb_player = _player_game_object.transform.GetComponent<Rigidbody>( );
 
         if ( Input.GetKey( KeyCode.UpArrow ) ) {
             z += speed;
@@ -65,9 +65,9 @@ public class script_Controller : MonoBehaviour {
         if ( Input.GetKey( KeyCode.RightArrow ) ) {
             x += speed;
         }
-        _rb_player.AddForce( x, 0f, z, ForceMode.Impulse );
+        rb_player.AddForce( x, 0.0f, z, ForceMode.Impulse );
     }
-    bool overlappedItem( ) {
+    bool isOverlappedItem( ) {
         Vector3 player_pos = _player_game_object.transform.position;
         Vector3 item_pos = _item_game_object.transform.position;
         float dis = Vector3.Distance( player_pos, item_pos );
@@ -77,7 +77,7 @@ public class script_Controller : MonoBehaviour {
         return false;
     }
     void removeItem( ) {
-        if ( overlappedItem( ) ) {
+        if ( isOverlappedItem( ) ) {
             Destroy( _item_game_object );
         }
     }
