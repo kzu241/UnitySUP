d6bac10b3b6467c848372c9e121267f5cc102f14
kzu241 <67955570+kzu241@users.noreply.github.com>
2022-02-11 12:48:30 +0900

    ohashi

--- MiniGame/Assets/Script/script_Controller.cs
+++ MiniGame/Assets/Script/script_Controller.cs
@@ -12,48 +12,48 @@ public class script_Controller : MonoBehaviour {
         return Instantiate( prefab_data, pos, Quaternion.identity );
     }
     void Start( ) {
-        GameObject _floor_game_object = loadPrefab( "prefab_Floor", new Vector3( 5.0f, 0.5f, 5.0f ) );
-        GameObject _front_game_object = loadPrefab( "prefab_FrontWall", new Vector3( 5.0f, 1.0f, 15.0f ) );
-        GameObject _back_game_object = loadPrefab( "prefab_BackWall", new Vector3( 5.0f, 1.0f, -5.0f ) );
-        GameObject _right_game_object = loadPrefab( "prefab_RightWall", new Vector3( 15.0f, 1.0f, 5.0f ) );
-        GameObject _left_game_object = loadPrefab( "prefab_LeftWall", new Vector3( -5.0f, 1.0f, 5.0f ) );
+        GameObject floor = loadPrefab( "prefab_Floor", new Vector3( 5.0f, 0.5f, 5.0f ) );
+        GameObject front = loadPrefab( "prefab_FrontWall", new Vector3( 5.0f, 1.0f, 15.0f ) );
+        GameObject back = loadPrefab( "prefab_BackWall", new Vector3( 5.0f, 1.0f, -5.0f ) );
+        GameObject right = loadPrefab( "prefab_RightWall", new Vector3( 15.0f, 1.0f, 5.0f ) );
+        GameObject left = loadPrefab( "prefab_LeftWall", new Vector3( -5.0f, 1.0f, 5.0f ) );
         _player_game_object = loadPrefab( "prefab_Player", new Vector3( 1.0f, 5.0f, 0.0f ) );
         _item_game_object = loadPrefab( "prefab_Item", new Vector3( 5.0f, 1.5f, 5.0f ) );
 
         _player_game_object.GetComponent<Renderer>( ).material.color = Color.red;
-        _front_game_object.GetComponent<Renderer>( ).material.color = Color.cyan;
-        _back_game_object.GetComponent<Renderer>( ).material.color = Color.cyan;
-        _right_game_object.GetComponent<Renderer>( ).material.color = Color.cyan;
-        _left_game_object.GetComponent<Renderer>( ).material.color = Color.cyan;
+        front.GetComponent<Renderer>( ).material.color = Color.cyan;
+        back.GetComponent<Renderer>( ).material.color = Color.cyan;
+        right.GetComponent<Renderer>( ).material.color = Color.cyan;
+        left.GetComponent<Renderer>( ).material.color = Color.cyan;
         _item_game_object.GetComponent<Renderer>( ).material.color = Color.yellow;
 
-        _left_game_object.transform.Rotate( 0.0f, 90.0f, 0.0f );
-        _right_game_object.transform.Rotate( 0.0f, 90.0f, 0.0f );
+        left.transform.Rotate( 0.0f, 90.0f, 0.0f );
+        right.transform.Rotate( 0.0f, 90.0f, 0.0f );
         _item_game_object.transform.Rotate( 0.0f, 0.0f, 45.0f );
     }
    
     void Update( ) {
         updateItem( );
-        movePlayer( );
+        updatePlayer( );
     }
     void updateItem( ) {
-        if( _item_game_object != null ){
-            rotationItem( );
-            if (isOverlappedItem( ) ) {
-                removeItem();
+        if( _item_game_object != null ) {
+            rotateItem( );
+            if ( isOverlappedItem( ) ) {
+                removeItem( );
             }
         }
     }
-	void rotationItem( ) {
-        Quaternion vec_rotation = Quaternion.Euler( 0.0f, Time.time * 50.0f, 45.0f);
+	void rotateItem( ) {
+        Quaternion vec_rotation = Quaternion.Euler( 0.0f, Time.time * 50.0f, 45.0f );
         _item_game_object.transform.rotation = vec_rotation;
     }
-    void movePlayer( ) {
+    void updatePlayer( ) {
         Rigidbody rb_player = _player_game_object.transform.GetComponent<Rigidbody>( );
         float speed = 20.0f * Time.deltaTime;
         float x = 0.0f;
         float z = 0.0f;
-
+        //カメラの方角を見て移動する。
         if ( Input.GetKey( KeyCode.UpArrow ) ) {
             z += speed;
         }
@@ -69,6 +69,7 @@ public class script_Controller : MonoBehaviour {
         rb_player.AddForce( x, 0.0f, z, ForceMode.Impulse );
     }
     bool isOverlappedItem( ) {
+        //OnCollisionEnterを使う。
         Vector3 player_pos = _player_game_object.transform.position;
         Vector3 item_pos = _item_game_object.transform.position;
         float dis = Vector3.Distance( player_pos, item_pos );
